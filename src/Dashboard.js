0bffdf8e78bdc09f911ef6608c785952cbc12aa1
HamidrezaRamezani <sena.hamidreza@gmail.com>
2021-12-12 21:14:51 +0330

    material ui added

---
+++ src/Dashboard.js
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+
+function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+}
+
+export default Dashboard;
