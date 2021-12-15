39043e65807a9c4da344fa7473b68ff89e06efd8
HamidrezaRamezani <sena.hamidreza@gmail.com>
2021-12-15 21:04:20 +0330

    New card added into the dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,11 +2,13 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import AppBar from './AppBar';
 import ClippedDrawer from './Drawer';
+import Content from './Content';
 
 function Dashboard() {
   return (
     <Box sx={{ display: 'flex' }}>
         <AppBar/>
+        <Content/>
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
             <ClippedDrawer/>
         </Box>
